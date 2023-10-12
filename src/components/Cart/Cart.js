d2b4d72d03763e60ce984653de074d01e1c62427
shefcodev <sherefjimoh@gmail.com>
2023-10-12 17:02:10 +0100

    added Cart component to ui

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import styles from 'Cart.module.css';
+
+const Cart = () => {
+  const cartItems = (
+    <ul className={styles['cart-items']}>
+      {[{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 }].map(
+        ({ id, name, amount, price }) => (
+          <li>{name}</li>
+        )
+      )}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>30.99</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles['button--alt']}>Close</button>
+        <button className={styles.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
